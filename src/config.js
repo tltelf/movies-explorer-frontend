6a65502f0fc4578a1a4e29520a849a52268a817d
Kirill Lyashenko <tltelf@yandex.ru>
2024-01-08 01:21:29 +0400

    Изменен URL адрес сервера

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL_API = 'https://api.tltelf.nomoredomains.rocks';
+export const BASE_URL_API = 'https://movies-explorer-api-wheat.vercel.app';
 
 export const URL_MOVIE_API = 'https://api.nomoreparties.co/beatfilm-movies';
