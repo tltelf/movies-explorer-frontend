098948509aa6165a06cffda3d93896e5ab2b3df0
Kirill Lyashenko <tltelf@yandex.ru>
2024-01-22 17:24:57 +0400

    Обновлена информация в разделе "о себе"

--- src/components/Main/AboutMe/AboutMe.jsx
+++ src/components/Main/AboutMe/AboutMe.jsx
@@ -1,33 +1,34 @@
 import { Section } from '../../Section/Section';
-import avatar from '../../../images/about-me__img.png';
+import avatar from '../../../images/about-me__img.jpg';
 
 export const AboutMe = () => {
-  return (
-    <Section
-      title={'Студент'}
-      className={'section_about-me'}
-      classNameContainer={'section__container_about-me'}
-    >
-      <div className='about-me__container'>
-        <h2 className='about-me__title'>Виталий</h2>
-        <p className='about-me__subtitle'>Фронтенд-разработчик, 30 лет</p>
-        <p className='about-me__text'>
-          Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня
-          есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом.
-          Недавно начал кодить. С 2015 года работал в компании «СКБ Контур».
-          После того, как прошёл курс по веб‑разработке, начал заниматься
-          фриланс-заказами и ушёл с постоянной работы.
-        </p>
-        <a
-          target='_blank'
-          href='https://github.com/tltelf'
-          rel='noopener noreferrer'
-          className='about-me__link'
-        >
-          Github
-        </a>
-        <img src={avatar} alt='Аватар' className='about-me__avatar' />
-      </div>
-    </Section>
-  );
+	return (
+		<Section
+			title={'Студент'}
+			className={'section_about-me'}
+			classNameContainer={'section__container_about-me'}
+		>
+			<div className='about-me__container'>
+				<h2 className='about-me__title'>Марк</h2>
+				<p className='about-me__subtitle'>
+					Фронтенд-разработчик, 27 лет
+				</p>
+				<p className='about-me__text'>
+					Я родился и живу в Тольятти. Я люблю слушать музыку, а ещё
+					увлекаюсь бегом. Недавно начал кодить. С 2018 года работал в
+					компании «Adidas». После того, как прошёл курс по
+					веб‑разработке, занялся поиском работы.
+				</p>
+				<a
+					target='_blank'
+					href='https://github.com/tltelf'
+					rel='noopener noreferrer'
+					className='about-me__link'
+				>
+					Github
+				</a>
+				<img src={avatar} alt='Аватар' className='about-me__avatar' />
+			</div>
+		</Section>
+	);
 };
